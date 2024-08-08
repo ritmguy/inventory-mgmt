e7e545b734b7e6220d092dfe294903af6d4ff984
chris <chris@protect247.com>
2024-08-08 14:02:27 +0000

    Adjusted README.

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 <p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
-<h1 align="center">Inventory Management System</h1>
+<h1 align="center">Laravel based Inventory Management System</h1>
 <hr>
 
 
 ## Quick Installation
 
-    git clone https://github.com/ekramasif/Inventory_Management_System.git
+    git clone https://github.com/ritmguy/inventory-mgmt.git
 
-    cd Inventory_Management_System
+    cd inventory-mgmt
     
 ### Composer
 
@@ -19,7 +19,7 @@
     cp .env.example .env
  
     
- ### For Migration table in database [Create database name as ```IMS```]
+### For Migration table in database [Create database name as ```IMS```]
  
     php artisan migrate
     
@@ -27,8 +27,4 @@
 
     php artisan serve
 
-## Our valuable team👩‍💻👨‍💻 :
 
-<p align="center">
-  <img src="https://contributors-img.web.app/image?repo=ekramasif/Inventory_Management_System" />
-</p>
--- resources/views/admin-dashboard.blade.php
+++ resources/views/admin-dashboard.blade.php
@@ -9,13 +9,13 @@
             <li class="breadcrumb-item active">Dashboard</li>
         </ol>
         <div class="row">
-            <div class="col-xl-3 col-md-6">
+            <div class="col-xl-5 col-md-6">
                 <div class="card bg-primary text-white mb-4">
                     <div class="card-body">
                         Laptops
                         <div class="row">
 
-                            <div class="col-xs-2 px-6 py-4">
+                            <div class="col-xs-2 px-4 py-4">
                                 @foreach($products as $product)
                                 @if($product->category != 'Laptops')
                                 @else 
@@ -37,13 +37,13 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-3 col-md-6">
+            <div class="col-xl-5 col-md-6">
                 <div class="card bg-warning text-white mb-4">
                     <div class="card-body">
                         Headsets
                         <div class="row">
 
-                            <div class="col-xs-2 px-6 py-4">
+                            <div class="col-xs-2 px-4 py-4">
                                 @foreach($products as $product)
                                 @if($product->category != 'Headset')
                                 
@@ -65,7 +65,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-3 col-md-6">
+            <!--<div class="col-xl-3 col-md-6">
                 <div class="card bg-success text-white mb-4">
                     <div class="card-body">Available Products</div>
                     <div class="card-footer d-flex align-items-center justify-content-between">
@@ -74,7 +74,7 @@
                     </div>
                 </div>
             </div>
-            <!-- <div class="col-xl-3 col-md-6">
+             <div class="col-xl-3 col-md-6">
                 <div class="card bg-danger text-white mb-4">
                     <div class="card-body">Pending Orders</div>
                     <div class="card-footer d-flex align-items-center justify-content-between">
